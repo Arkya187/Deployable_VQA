4afcd880ac59c413360a59024722bcefe8bc9f61
Arkya Chatterjee <arkyachatterjee8@gmail.com>
2026-02-11 16:36:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import tensorflow as tf
+import pickle
+import cv2
+import numpy as np
+import gdown
+import os
+
+MODEL_URL = "https://drive.google.com/file/d/1c9rri9R44zQ7eEvC97hxvjsvfLXyn25z/view?usp=sharing"
+
+MODEL_PATH = "vqa_model.keras"
+
+@st.cache_resource
+def load_model():
+
+    if not os.path.exists(MODEL_PATH):
+        st.info("Downloading model... please wait")
+        gdown.download(MODEL_URL, MODEL_PATH, quiet=False)
+
+    return tf.keras.models.load_model(MODEL_PATH)
+
+model = load_model()
+tokenizer = pickle.load(open("tokenizer.pkl","rb"))
+
+MAX_LEN = 20
+
+st.title("Medical VQA")
+
+uploaded = st.file_uploader("Upload Image")
+
+question = st.text_input("Enter Question")
+
+if uploaded and question:
+
+    file_bytes = np.asarray(bytearray(uploaded.read()), dtype=np.uint8)
+    image = cv2.imdecode(file_bytes, 1)
+    image = cv2.resize(image, (256,256))
+    image = tf.keras.applications.resnet50.preprocess_input(image)
+    image = np.expand_dims(image, axis=0)
+
+    seq = tokenizer.texts_to_sequences([question])
+    seq = tf.keras.utils.pad_sequences(seq, maxlen=MAX_LEN)
+
+    pred = model.predict([image, seq])[0][0]
+
+    answer = "YES" if pred > 0.5 else "NO"
+
+    st.write("Prediction:", answer)
